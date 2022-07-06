fb288a1dfd639530b0a60c09aea95a078fbf1cd0
HalynaStep <gstep2003@gmail.com>
2022-07-06 19:45:40 +0300

    update-task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
 
+
+const startBtnEl = document.querySelector('[data-start]');
+const stopBtnEl = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector("body")
+let intervalId = null;
+
+startBtnEl.addEventListener('click', onStart)
+stopBtnEl.addEventListener('click', onStop);
+
+function onStart(event) {
+    startBtnEl.disabled = event.target;
+    intervalId = setInterval(() => { bodyEl.style.backgroundColor = getRandomHexColor() }, 1000); 
+};
+
+
+
+
+function onStop(event) {
+    startBtnEl.disabled = !event.target;
+    clearInterval(intervalId);
+}
+
+ function getRandomHexColor() {
+     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+ }
+
+ 
