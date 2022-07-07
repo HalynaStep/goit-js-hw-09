757bf8d8d89c5cd244779b89c60d470503aa25f2
HalynaStep <gstep2003@gmail.com>
2022-07-07 20:21:17 +0300

    update-task-02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,14 +4,16 @@ import Notiflix from 'notiflix';
 
 const startBtnEl = document.querySelector('[data-start]');
 const dateInputEl = document.querySelector('#datetime-picker');
-let dataDaysEl = document.querySelector('[data-days]');
-let dataHoursEl = document.querySelector('[data-hours]');
-let dataMinutesEl = document.querySelector('[data-minutes]');
-let dataSecondsEl = document.querySelector('[data-seconds]');
+const dataDaysEl = document.querySelector('[data-days]');
+const dataHoursEl = document.querySelector('[data-hours]');
+const dataMinutesEl = document.querySelector('[data-minutes]');
+const dataSecondsEl = document.querySelector('[data-seconds]');
 
-let targetDates = null;
+let targetDate = null;
 let timerId = null;
-let diff = null;
+let diffTime = null;
+
+
 
 
 const options = {
@@ -19,46 +21,33 @@ const options = {
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  onClose(selectedDates) {
-      if (selectedDates[0] < Date.now()) {
+    onClose(selectedDates) {
+      targetDate = selectedDates[0].getTime();
+      if (targetDate < Date.now()) {
           Notiflix.Notify.failure('Please choose a date in the future');
       }
       else {
-          targetDates = selectedDates[0];
-          startBtnEl.style.disebled=true;
+          startBtnEl.removeAttribute('disabled');
       }
   },
 };
 
-startBtnEl.style.disebled=false;
-flatpickr(dateInputEl, {options});
+startBtnEl.setAttribute('disabled', true);
+flatpickr(dateInputEl, options);
 startBtnEl.addEventListener('click', onStartTime)
 
-function onStartTime(event) {      
-event.preventDefault();
-   timerId = setInterval (() =>{
-   diff = targetDates - Date.now();
-        if(diff<0) {
+function onStartTime(event) {
+    startBtnEl.setAttribute('disabled', true);
+    timerId = setInterval(() => {
+        diffTime = targetDate - Date.now();
+        const diffDate = convertMs(diffTime);
+        if (diffDate.seconds < 0) {
             clearInterval(timerId);
             return;
         }
-    addTextContetnt(diff);
-        
-    },1000) 
-}
-
-function addLeadingZero(value) {
-    return String(value).padStart(2,'0');
-
-}
-
-function addTextContetnt(diff){
-    dataDaysEl.textContent = addLeadingZero(convertMs(diff).days);
-    dataHoursEl.textContent = addLeadingZero(convertMs(diff).hours);
-    dataMinutesEl.textContent = addLeadingZero(convertMs(diff).minutes);
-    dataSecondsEl.textContent = addLeadingZero(convertMs(diff).seconds);
-}
-
+        addTextContetnt(diffDate);
+    }, 1000)
+};
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -77,8 +66,19 @@ function convertMs(ms) {
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
-}
+};
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+function addLeadingZero(value) {
+    return String(value).padStart(2,'0');
+
+};
+
+function addTextContetnt({ days, hours, minutes, seconds }){
+    dataDaysEl.textContent = addLeadingZero(days);
+    dataHoursEl.textContent = addLeadingZero(hours);
+    dataMinutesEl.textContent = addLeadingZero(minutes);
+    dataSecondsEl.textContent = addLeadingZero(seconds);
+};
+
+
+
